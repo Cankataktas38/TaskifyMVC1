3e2485fc30e5296599fec9fa26e2b95cf0f052b9
Arşın <Arn@DESKTOP-TN1OHBO>
2025-10-02 19:24:27 +0300

    Proje dosyası ekle.

---
+++ TaskifyMVC/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using TaskifyMVC.Models;
+namespace TaskifyMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IHttpClientFactory? _factory;
+        public AccountController(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username,string password)
+        {
+            var client = _factory.CreateClient("TaskifyAPI");
+            var payload = JsonConvert.SerializeObject(new { Username = username, Password = password });
+            var res = await client.PostAsync("api/auth/login",new StringContent(payload,Encoding.UTF8, "application/json"));
+            if (!res.IsSuccessStatusCode)
+            {
+                ViewBag.Error = "Giriş başarısız";
+                return View();
+            }
+            var json = await res.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<dynamic>(json);
+            string token = obj.token;
+            HttpContext.Session.SetString("JWToken",token);
+            return RedirectToAction("Index","Task");
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("JWToken");
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ TaskifyMVC/Controllers/TaskController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using TaskifyMVC.Models;
+namespace TaskifyMVC.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly IHttpClientFactory? _factory;
+        public TaskController(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+        private HttpClient GetClient()
+        {
+            var client = _factory.CreateClient("TaskifyAPI");
+            var token = HttpContext.Session.GetString("JWToken");
+            if(!string.IsNullOrEmpty(token) )
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",token);
+            }
+            return client;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = GetClient();
+            var res = await client.GetAsync("api/tasks");
+            List<TaskViewModel> tasks = new();
+            if (res.IsSuccessStatusCode)
+            {
+               var json = await res.Content.ReadAsStringAsync();
+                tasks = JsonConvert.DeserializeObject<List<TaskViewModel>>(json);
+            }
+            ViewBag.IsAuthenticated = !string.IsNullOrEmpty(HttpContext.Session.GetString("JWToken"));
+                return View(tasks);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskViewModel model)
+        {
+            var client = GetClient();
+            var payload = JsonConvert.SerializeObject(new { Title = model.Title,Description = model.Description });
+            await client.PostAsync("api/tasks", new StringContent(payload, Encoding.UTF8, "application/json"));
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = GetClient();
+            await client.DeleteAsync($"api/tasks/{id}");
+            return RedirectToAction("Index");
+        }
+    }
+}
